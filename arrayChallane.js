a5e8d9e6bad0713da4558a192d9d7383a15df201
meparishvili.i <Iveri.Meparishvili@sa.gov.ge>
2022-10-28 21:00:30 +0400

    add

--- arrayChallane.js
+++ arrayChallane.js
@@ -2,7 +2,6 @@ function WordSplit([word, dictionary]) {
     dictionary = new Set(dictionary.split(","));
     for (let i = 0; i < word.length; i++) {
       if (dictionary.has(word.slice(0, i)) && dictionary.has(word.slice(i))) {
-        console.log(word, 'can be split into', word.slice(0, i), 'and', word.slice(i));
         return true;
       }
     }
