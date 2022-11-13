dd4628a6f581960cba7ec9f6447b2b211d20ebaa
meparishvili.i <Iveri.Meparishvili@sa.gov.ge>
2022-11-13 13:27:29 +0400

    spiral matrix

---
+++ spiralMatrix.js
@@ -0,0 +1,32 @@
+var spiralOrder = function(matrix) {
+    var left = 0;
+    var right = matrix[0].length-1;
+    var top = 0;
+    var bottom = matrix.length-1;
+    
+    var answerList = [];
+    
+    while(left <= right && top <= bottom) {
+        for(let i =left; i<=right; i++) {
+            answerList.push(matrix[top][i]);
+        }
+        top++;
+        
+        for(let i =top; i<=bottom; i++) {
+            answerList.push(matrix[i][right]);
+            
+        }
+        right--;
+        
+        for(let i =right; i>=left; i--) {
+            answerList.push(matrix[bottom][i]);
+        }
+        bottom--;
+    
+        for(let i =bottom; i>=top; i--) {
+            answerList.push(matrix[i][left]);
+        }
+        left++;
+    }
+    return answerList;
+};
