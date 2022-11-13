e7960b329a2215c081a435aa43400697d5f17c9b
meparishvili.i <Iveri.Meparishvili@sa.gov.ge>
2022-11-13 13:48:31 +0400

    reverse words

---
+++ reverseWordsInString.js
@@ -0,0 +1,15 @@
+function reverseWords(s) {
+    const ret = [];
+    let word = [];
+    for (let i = 0; i < s.length; ++i) {
+      if (s.charAt(i) === ' ') {
+          word.length > 0 && ret.unshift(word.join(''));
+          word = [];
+        }
+      else {
+        word.push(s.charAt(i));
+      }
+    }
+    word.length > 0 && ret.unshift(word.join(''));
+    return ret.join(' ');
+};
