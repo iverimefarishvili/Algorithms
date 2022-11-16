4159f47aee0bfb2470fccd46343ed393c7e519f2
iverimefarishvili <mefarishvili922@gmail.com>
2022-11-16 20:54:39 +0400

    longest substring

---
+++ longestSubstring.js
@@ -0,0 +1,14 @@
+var lengthOfLongestSubstring = function(s) {
+    const seen = new Map();
+    let start = 0;
+    let maxLen = 0;
+    
+    for(let i = 0; i < s.length; i++) {
+        if(seen.has(s[i])) start = Math.max(seen.get(s[i]) + 1, start)
+        seen.set(s[i], i);
+
+        maxLen = Math.max(i - start + 1, maxLen);
+    } 
+    
+    return maxLen;  
+};
